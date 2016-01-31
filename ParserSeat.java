8e74cae148876f85c6ff0ba359bb7222cf04ee95
Victor Sokolov <70victor@mail.ru>
2016-01-31 07:47:47 +0300

    My version Test Java Programm

--- ParserSeat.java
+++ ParserSeat.java
@@ -1,10 +1,11 @@
 package test;
 
 import java.util.HashMap;
-import java.util.regex.*;
+// import java.util.Collections; // Это если второй вариант таблицы перекодировки
+import java.util.regex.*; // import java.util.regex.Pattern.*; // загружаем компоненты по минимуму
 
 public class ParserSeat {
-    private static HashMap<String, String> misprints = new HashMap<String, String>() {{
+    private final static HashMap<String, String> misprints = new HashMap<String, String>() {{ // Map это пара: Ключ+Значение, ключ по любому короче значения ?Замена рус./лат.
         put("С", "C");
         put("Е", "E");
         put("Т", "T");
@@ -18,35 +19,53 @@ public class ParserSeat {
         put("К", "K");
         put("М", "M");
     }};
+    /* Так будет совсем неизменяемая 
+    private static final HashMap<Character, Character> misprints; // Character должен быть меньше String, но возможно преобразования типов
+    static {
+        HashMap aMap = new HashMap();        
+        aMap.put("С", "C");        aMap.put("Е", "E");        aMap.put("Т", "T");        aMap.put("Н", "H");
+        aMap.put("У", "Y");        aMap.put("О", "O");        aMap.put("Р", "P");        aMap.put("Х", "X");
+        aMap.put("А", "A");        aMap.put("В", "B");        aMap.put("К", "K");        aMap.put("М", "M");
+        misprints = (HashMap)Collections.unmodifiableMap(aMap);
+    }    
+    */
 
-    public ParserSeat(){
-
-    }
+    public ParserSeat(){ } // ИМХО зачем пустые строки!? Лучше писать покороче. Любая идея должна излагаться на одной странице - говорили в IBM.Весь код должен помещаться на одном экране - Forth
 
     public static ResultParser parser(String seatName, long id){
-        ResultParser result = new ResultParser();
-        result.setId(id);
-        Matcher m = Pattern.compile("(?:(?<sector>.*?) (?<sectorName>(?:\\p{Lu}|\\d).*?)|(?<sectorWithoutName>.*)) (?<row>Ряд) (?<rowName>.*) (?<seat>Место) (?<seatName>.*)").matcher(seatName);
-        if (m.find()) {
+        ResultParser result = new ResultParser(id);
+       /*
+        Вот в регулярных выражениях я ваащще не рублю, предположу, что исходный код работает правильно
+        Matcher m = Pattern.compile("(?:(?<sectorId>.*?) (?<sectorName>(?:\\p{Lu}|\\d).*?)|(?<sectorWithoutName>.*)) (?<rowId>Ряд) (?<rowName>.*) (?<seatId>Место) (?<seatName>.*)").matcher(seatName);
+        или
+        String s = "(?:(?<sector>.*?) (?<sectorName>(?:\\p{Lu}|\\d).*?)|(?<sectorWithoutName>.*)) (?<row>Ряд) (?<rowName>.*) (?<seat>Место) (?<seatName>.*)";
+        Pattern p = Pattern.compile(s, Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(seatName);
+       */
+        Matcher m = Pattern.compile("(?:(?<sectorId>.*?) (?<sectorName>(?:\\p{Lu}|\\d).*?)|(?<sectorWithoutName>.*)) (?<rowId>Ряд) (?<rowName>.*) (?<seatId>Место) (?<seatName>.*)",
+                Pattern.CASE_INSENSITIVE).matcher(seatName);
+        if (m.find()) { // m.matches()
             if (m.group("sectorWithoutName") != null) {
-                result.setSector(m.group("sectorWithoutName"));
+                result.setSectorId(m.group("sectorWithoutName"));
                 result.setSectorName("");
             } else {
-                result.setSector(m.group("sector"));
+                result.setSectorId(m.group("sectorId"));
                 result.setSectorName(m.group("sectorName"));
             }
-            result.setRow(m.group("row"));
+            result.setRowId(m.group("rowId"));
             result.setRowName(m.group("rowName"));
-            result.setSeat(m.group("seat"));
+            result.setSeatId(m.group("seatId"));
             result.setSeatName(m.group("seatName"));
         }
-        if (result.getSector() == null || result.getRow() == null || result.getRowName() == null || result.getSeat() == null || result.getSeatName() == null) {
+       /*
+        Если я прав, насчёт Id сектора, ряда и места, то проверять Name не надо
+        if (result.getSectorId() == null || result.getRowId() == null || result.getRowName() == null || result.getSeatId() == null || result.getSeatName() == null)
+       */
+        if (result.getSectorId() == null || result.getRowId() == null || result.getSeatId() == null)
             return  null;
-        }
-        for (String cyrillicCharacter : misprints.keySet()) {
+        for (String cyrillicCharacter : misprints.keySet())
             result.setSectorName(result.getSectorName().replace(cyrillicCharacter, misprints.get(cyrillicCharacter)));
-        }
+        // А, не нужно ли и остальные поля с именами аналогично модифицировать?
         return result;
     }
-
 }
--- ResultParser.java
+++ ResultParser.java
@@ -1,91 +1,64 @@
 package test;
 
 public class ResultParser {
-  private long id;
-  private String sector;
-  private String sectorName;
-  private String row;
-  private String rowName;
-  private String seat;
-  private String seatName;
+  private long id; // Вероятно, id не может не существовать поэтому лучше конструктор с заданием значения id сразу и финализация переменной
+  private String                // Вероятно пары значений соответствуют друг-другу. Id обязателен, имя (описание) может отсутствовать
+          sectorId, sectorName, // сектор
+          rowId,    rowName,    // ряд
+          seatId,   seatName;   // место
+  // Не должны ли места иметь дополнительные данные в описании? Например, номер ближайшего выхода!?
 
-  public long getId() {
-    return id;
-  }
+public ResultParser(){ }        // Конструктор по умолчанию
+public ResultParser(long IdRP){ // Конструктор с параметром, возможно объекты без Id не имеют смысла
+	this.id = IdRP;
+}
+		  
+  // ИМХО зачем пустые строки!? Лучше писать покороче. Любая идея должна излагаться на одной странице - говорили в IBM.Весь код должен помещаться на одном экране - Forth
+  public void setId(long id) { this.id = id; } // возможно id устанавливается единожды, при создании и теоретически не должно меняться final, тогда метод не нужен
+  public long getId() { return this.id; }
+  
+ /*
+  Лучше сразу приводить к единообразию при вводе и не преобразовывать каждый раз при проверке
+  А, ещё лучше создать множества допустимых значений, для каждого поля (если это возможно).
+  Например, public enum Day { SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY } Можно коллекцию в .ini файле
+ */
+ /*
+  Если *Id соответствует *Name, то правильнее:
+  public void setSector(String SectorId, String SectorName) { sectorId = normStr(SectorId); sectorName = normStr(SectorName); }
+  public String getSectorId()   { return this.sectorId; }
+  public String getSectorName() { return this.sectorName; }
+ */ 
+  public void setSectorId(String sctrId) { this.sectorId = normStr(sctrId); }
+  public String getSectorId() { return this.sectorId; }
 
-  public void setId(long id) {
-    this.id = id;
-  }
+  public void setSectorName(String sctrName) { this.sectorName = normStr(sctrName); } // Лучше сразу приводить к единообразию при вводе и не преобразовывать при каждой проверке
+  public String getSectorName() { return this.sectorName; }
 
-  public String getSector() {
-    return sector;
-  }
+  public void setRowId(String rwId) { this.rowId = normStr(rwId); }
+  public String getRowId() { return this.rowId; }
+  
+  public void setRowName(String rwName) { this.rowName = normStr(rwName); } // Лучше сразу приводить к единообразию при вводе и не преобразовывать при каждой проверке
+  public String getRowName() { return this.rowName; }
+  
+  public void setSeatId(String stId) { this.seatId = normStr(stId); } // Лучше сразу приводить к единообразию при вводе и не преобразовывать при каждой проверке
+  public String getSeatId() { return this.seatId; }
+  
+  public void setSeatName(String stName) { this.seatName = normStr(stName); } // Лучше сразу приводить к единообразию при вводе и не преобразовывать при каждой проверке
+  public String getSeatName() { return this.seatName; }
+  
+/*
+  Думаю сектор, ряд или место может быть без имени (с пустым именем), но без идентификатора быть не может, а тогда условие можно упростить
+  public String getFullSector() { return sector != null && sectorName != null ? toString(sector, sectorName) : "";} // Думаю этот метод ничем не должен отличаться от следующих, а все необходимое можно собрать в методе toString()
+  public String getFullRow() { return row != null && rowName != null ? toString(row, rowName) : ""; }
+  public String getFullSeat() { return seat != null && seatName != null ? toString(seat, seatName) : ""; }
+*/
+  // Практически ? врядли отличается от if
+  public String getFullSector() { return this.sectorId != null ? toString(this.sectorId, this.sectorName) : "";} // Думаю этот метод ничем не должен отличаться от следующих, а все необходимое можно собрать в методе toString()
+  public String getFullRow() { return this.rowId != null ? toString(this.rowId, this.rowName) : ""; }
+  public String getFullSeat() { return this.seatId != null ? toString(this.seatId, this.seatName) : ""; }
 
-  public void setSector(String sector) {
-    this.sector = sector;
-  }
-
-  public String getSectorName() {
-    return sectorName;
-  }
-
-  public void setSectorName(String sectorName) {
-    this.sectorName = sectorName;
-  }
-
-  public String getRow() {
-    return row;
-  }
-
-  public void setRow(String row) {
-    this.row = row;
-  }
-
-  public String getRowName() {
-    return rowName;
-  }
-
-  public void setRowName(String rowName) {
-    this.rowName = rowName;
-  }
-
-  public String getSeat() {
-    return seat;
-  }
-
-  public void setSeat(String seat) {
-    this.seat = seat;
-  }
-
-  public String getSeatName() {
-    return seatName;
-  }
-
-  public void setSeatName(String seatName) {
-    this.seatName = seatName;
-  }
-
-  public String getFullSector() {
-    if (sector != null && sectorName != null) {
-      if (sector.equalsIgnoreCase("сектор")) {
-        return sectorName;
-      }
-      return sector + " " + sectorName;
-    }
-    return "";
-  }
-
-  public String getFullRow() {
-    if (row != null && rowName != null) {
-      return row + " " + rowName;
-    }
-    return "";
-  }
-
-  public String getFullSeat() {
-    if (seat != null && seatName != null) {
-      return seat + " " + seatName;
-    }
-    return "";
-  }
+  private String normStr(String str) { return str.toLowerCase(); } // Общий для всех полей вид. Лучше сразу приводить к какому-то единообразию при вводе
+  private String toString(String prm, String prmNm) { // Единообразное форматирование данных. Возможно метод надо развить до вывода всей информации объекта
+	return prm + " " + prmNm; // Или return formatter.format("%S%n %S%n", prm, prmNm) (тут можно ещё учесть Locale); но тогда надо будет догрузить библиотеку
+  }  
 }
