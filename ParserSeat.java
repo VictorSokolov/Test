3c6da029f11899eb76a093b194d33c5e3290867c
Victor Sokolov <70victor@mail.ru>
2016-01-31 21:30:59 +0300

    31.01.16 Вынести вверх компиляцию паттерна это гут. Может ещё автозамену (лат/рус) оптимизировать!? HashMap (Character, Character) вместо стринг и отказаться от replace()

--- ParserSeat.java
+++ ParserSeat.java
@@ -3,50 +3,38 @@ package test;
 import java.util.HashMap;
 import java.util.regex.*;
 
-public class ParserSeat {
-    private static HashMap<String, String> misprints = new HashMap<String, String>() {{
-        put("С", "C");
-        put("Е", "E");
-        put("Т", "T");
-        put("Н", "H");
-        put("У", "Y");
-        put("О", "O");
-        put("Р", "P");
-        put("Х", "X");
-        put("А", "A");
-        put("В", "B");
-        put("К", "K");
-        put("М", "M");
+public class ParserSeat { // !
+    private final static HashMap<String, String> misprints = new HashMap<String, String>() {{
+        put("С", "C"); put("Е", "E"); put("Т", "T"); put("Н", "H"); put("У", "Y"); put("О", "O");
+        put("Р", "P"); put("Х", "X"); put("А", "A"); put("В", "B"); put("К", "K"); put("М", "M");
     }};
-
-    public ParserSeat(){
-
+    
+     public static String fastReplace(String word) { // !!!
+         for (String cyrillicCharacter : misprints.keySet())
+             word.replace(cyrillicCharacter, misprints.get(cyrillicCharacter));
+         return word;
     }
+    
+    private static final Pattern p = Pattern.compile(
+            "(?:(?<sector>.*?) (?<sectorName>(?:\\p{Lu}|\\d).*?)|(?<sectorWithoutName>.*)) (?<row>Ряд) (?<rowName>.*) (?<seat>Место) (?<seatName>.*)", Pattern.CASE_INSENSITIVE);
+    
+    public ParserSeat(){ }
 
-    public static ResultParser parser(String seatName, long id){
-        ResultParser result = new ResultParser();
-        result.setId(id);
-        Matcher m = Pattern.compile("(?:(?<sector>.*?) (?<sectorName>(?:\\p{Lu}|\\d).*?)|(?<sectorWithoutName>.*)) (?<row>Ряд) (?<rowName>.*) (?<seat>Место) (?<seatName>.*)").matcher(seatName);
-        if (m.find()) {
+    public static ResultParser parser(String seatName, long id) {
+        ResultParser result = new ResultParser(id);        
+        Matcher m = p.matcher(seatName);
+        if (m.find())
             if (m.group("sectorWithoutName") != null) {
-                result.setSector(m.group("sectorWithoutName"));
-                result.setSectorName("");
+                result.setSectorId(m.group("sectorWithoutName")); result.setSectorName("");
             } else {
-                result.setSector(m.group("sector"));
-                result.setSectorName(m.group("sectorName"));
+                result.setSectorId(m.group("sectorId")); result.setSectorName(m.group("sectorName"));
+                result.setRowId(m.group("rowId"));   result.setRowName(m.group("rowName"));
+                result.setSeatId(m.group("seatId")); result.setSeatName(m.group("seatName"));
             }
-            result.setRow(m.group("row"));
-            result.setRowName(m.group("rowName"));
-            result.setSeat(m.group("seat"));
-            result.setSeatName(m.group("seatName"));
-        }
-        if (result.getSector() == null || result.getRow() == null || result.getRowName() == null || result.getSeat() == null || result.getSeatName() == null) {
+        if (result.getSectorId() == null || result.getRowId() == null || result.getRowName() == null || result.getSeatId() == null || result.getSeatName() == null)
             return  null;
-        }
-        for (String cyrillicCharacter : misprints.keySet()) {
-            result.setSectorName(result.getSectorName().replace(cyrillicCharacter, misprints.get(cyrillicCharacter)));
-        }
+        for (String cyrillicCharacter : misprints.keySet())
+            result.setSectorName(fastReplace(result.getSectorName()));
         return result;
     }
-
 }
--- ResultParser.java
+++ ResultParser.java
@@ -1,91 +1,40 @@
 package test;
 
 public class ResultParser {
-  private long id;
-  private String sector;
-  private String sectorName;
-  private String row;
-  private String rowName;
-  private String seat;
-  private String seatName;
+  private long id; // Вероятно, id не может не существовать поэтому лучше конструктор с заданием значения id сразу и финализация переменной
+  private String                // Вероятно пары значений соответствуют друг-другу. Id обязателен, имя (описание) может отсутствовать
+          sectorId, sectorName, // сектор
+          rowId,    rowName,    // ряд
+          seatId,   seatName;   // место
 
-  public long getId() {
-    return id;
-  }
+public ResultParser(){ }                          // Конструктор по умолчанию
+public ResultParser(long IdRP){ id = IdRP; } // Конструктор с параметром, возможно объекты без Id не имеют смысла		  
+  
+  public void setId(long id) { this.id = id; } // возможно id устанавливается единожды, при создании и теоретически не должно меняться final, тогда метод не нужен
+  public long getId() { return id; }
+  
+  public void setSectorId(String sctrId) { this.sectorId = normStr(sctrId); }
+  public String getSectorId() { return sectorId; }
 
-  public void setId(long id) {
-    this.id = id;
-  }
+  public void setSectorName(String sctrName) { this.sectorName = normStr(sctrName); } // Лучше сразу приводить к единообразию при вводе и не преобразовывать при каждой проверке
+  public String getSectorName() { return sectorName; }
 
-  public String getSector() {
-    return sector;
-  }
+  public void setRowId(String rwId) { this.rowId = normStr(rwId); }
+  public String getRowId() { return rowId; }
+  
+  public void setRowName(String rwName) { this.rowName = normStr(rwName); } // Лучше сразу приводить к единообразию при вводе и не преобразовывать при каждой проверке
+  public String getRowName() { return rowName; }
+  
+  public void setSeatId(String stId) { this.seatId = normStr(stId); } // Лучше сразу приводить к единообразию при вводе и не преобразовывать при каждой проверке
+  public String getSeatId() { return seatId; }
+  
+  public void setSeatName(String stName) { this.seatName = normStr(stName); } // Лучше сразу приводить к единообразию при вводе и не преобразовывать при каждой проверке
+  public String getSeatName() { return seatName; }
 
-  public void setSector(String sector) {
-    this.sector = sector;
-  }
+  public String getFullSector() { return sectorId != null ? toString(sectorId, sectorName) : "";} // Думаю этот метод ничем не должен отличаться от следующих, а все необходимое можно собрать в методе toString()
+  public String getFullRow() { return rowId != null ? toString(rowId, rowName) : ""; }
+  public String getFullSeat() { return seatId != null ? toString(seatId, seatName) : ""; }
 
-  public String getSectorName() {
-    return sectorName;
-  }
-
-  public void setSectorName(String sectorName) {
-    this.sectorName = sectorName;
-  }
-
-  public String getRow() {
-    return row;
-  }
-
-  public void setRow(String row) {
-    this.row = row;
-  }
-
-  public String getRowName() {
-    return rowName;
-  }
-
-  public void setRowName(String rowName) {
-    this.rowName = rowName;
-  }
-
-  public String getSeat() {
-    return seat;
-  }
-
-  public void setSeat(String seat) {
-    this.seat = seat;
-  }
-
-  public String getSeatName() {
-    return seatName;
-  }
-
-  public void setSeatName(String seatName) {
-    this.seatName = seatName;
-  }
-
-  public String getFullSector() {
-    if (sector != null && sectorName != null) {
-      if (sector.equalsIgnoreCase("сектор")) {
-        return sectorName;
-      }
-      return sector + " " + sectorName;
-    }
-    return "";
-  }
-
-  public String getFullRow() {
-    if (row != null && rowName != null) {
-      return row + " " + rowName;
-    }
-    return "";
-  }
-
-  public String getFullSeat() {
-    if (seat != null && seatName != null) {
-      return seat + " " + seatName;
-    }
-    return "";
-  }
+  private String normStr(String str) { return str.toLowerCase(); } // Общий для всех полей вид. Лучше сразу приводить к какому-то единообразию при вводе
+  private String toString(String prm, String prmNm) { return prm + " " + prmNm; }  
 }
